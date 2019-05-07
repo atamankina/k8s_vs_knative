8393efc13112f85b44e09e53ad012a1b4311741f
Galina Atamankina <g.atamankina@gmail.com>
2019-05-07 23:27:52 +0200

    Fix manage.py.

--- environment/script.sh
+++ environment/script.sh
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 python manage.py db init
 python manage.py db migrate
 python manage.py db upgrade
-python run.py
+gunicorn --bind 0.0.0.0:5000 -w 4 wsgi:app
--- manage.py
+++ manage.py
@@ -1,13 +1,9 @@
-import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from src.models.UserModel import UserModel
 from src.models.ReviewModel import ReviewModel
 from src.models.RestaurantModel import RestaurantModel
-from src.app import create_app, db
-
-env_name = os.getenv('FLASK_ENV')
-app = create_app(env_name)
+from src.reviews_app import app, db
 
 migrate = Migrate(app=app, db=db, user=UserModel, review=ReviewModel, restaurant=RestaurantModel)
 
